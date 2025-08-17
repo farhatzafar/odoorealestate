1550f43afca5677929a04b16c91ea337e27e047d
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-17 13:38:53 -0400

    add sql and python constaints

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,11 +1,32 @@
 from datetime import timedelta
-from odoo import fields, models, _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-
+from odoo.tools import float_compare, float_is_zero
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Info on real estate properties"
+
+    # SQL constraints, expected price must be strictly positive, selling price must be positive
+    _sql_constraints = [
+        ('check_expected_price', 'CHECK(expected_price>0)',
+         'A property expected price must be strictly positive'),
+        ('check_selling_price', 'CHECK(selling_price>=0)',
+         'A property selling price must be positive')
+    ]
+
+    # Python constraint, selling price cannot be lower than 90% of the expected price
+    @api.constrains('selling_price', 'expected_price')
+    def _check_selling_price(self):
+        for record in self:
+            # returns True if selling price is zero
+            if float_is_zero(record.selling_price, precision_rounding=0.01):
+                continue
+            else:
+                # returns -1 : If the first value is less than the second value.
+                if float_compare(record.selling_price, 0.90*record.expected_price, precision_rounding=0.01) < 0:
+                    raise UserError(
+                        _("The selling price cannot be lower than 90 percent of the expected price."))
 
     name = fields.Char('Title', required=True)
     description = fields.Text('Description')
@@ -18,6 +39,17 @@ class EstateProperty(models.Model):
     living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
+
+    # Onchange function to set garden area to 10 and orientation to north when garden is set to true
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
@@ -28,12 +60,9 @@ class EstateProperty(models.Model):
     active = fields.Boolean('Active', default=True)
 
     state = fields.Selection(
-        string='State',
-        selection=[
-            ('new', 'New'), ('offer_received',
-                             'Offer Received'), ('offer_accepted', 'Offer_Accepted'),
-            ('sold', 'Sold'), ('cancelled', 'Cancelled')
-        ],
+        string='Status',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer_Accepted'),
+                   ('sold', 'Sold'), ('cancelled', 'Cancelled')],
         required=True, copy=False, default='new'
     )
 
