2884507d9256deda6b1abf8ddf38ee6c951539bb
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-19 15:58:01 -0400

    add res_users_views and model so available properties linked to a salesperson are displayed in settings->users&companies->users

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -1,6 +1,6 @@
 from odoo import fields, models
 
-class ResUsers(model.Model):
+class ResUsers(models.Model):
     _inherit = "res.users"
 
     property_ids = fields.One2many("estate.property", "user_id", string="Properties",
