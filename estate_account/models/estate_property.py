d9f05610617cbcb8cc9409bdf4baa0646b5bffeb
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-21 15:07:03 -0400

    add function to create invoice when property is sold

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, _
+from odoo.fields import Command
 
 class EstateProperty(models.Model):
     
@@ -6,5 +7,27 @@ class EstateProperty(models.Model):
 
 #Override the original sold button
     def _sold_button(self):
-        print("Testing that the sold action is working")
-        return super().sold_button()
+        res = super().sold_button()
+
+        for record in self:
+            if not record.buyer_id:
+                raise UserError(_("Cannot generate invoice without a buyer"))
+            
+            self.env['account.move'].create({
+            'partner_id': record.buyer_id.id,
+            'move_type': 'out_invoice',
+            'line_ids': [
+                Command.create({
+                    'name': record.name,
+                    'quantity': 1,
+                    'price_unit': record.selling_price * 0.06
+                }),
+                Command.create({
+                    'name': "Admin Fees",
+                    'quantity': 1,
+                    'price_unit': 100.00
+                })
+            ]
+            })
+
+        return res
